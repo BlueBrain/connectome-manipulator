4a2099b213191edbeb32950be2b3ad4ca4f59d1d
chr-pok <christoph.pokorny@epfl.ch>
2021-05-05 10:15:16 +0200

    Improved position mapping model

--- pipeline/model_building/pos_mapping.py
+++ pipeline/model_building/pos_mapping.py
@@ -15,8 +15,8 @@ from voxcell.nexus.voxelbrain import Atlas
 from scipy.interpolate import griddata
 from scipy.spatial import distance_matrix
 
-""" Extract position mapping from atlas space to flat space (2 files required: 1. xy mapping in a.u., 2. z (=depth) mapping in um) """
-def extract(circuit, flatmap_path, flatmap_files, xy_scale=None, **_):
+""" Extract position mapping from atlas space to flat space (2 files required: 1. xy mapping, 2. z (=depth) mapping) """
+def extract(circuit, flatmap_path, xy_file, z_file, xy_scale=None, z_scale=None, **_):
     
     # Get neuron positions
     nodes = circuit.nodes['All']
@@ -24,27 +24,34 @@ def extract(circuit, flatmap_path, flatmap_files, xy_scale=None, **_):
     nrn_ids = nrn_pos.index.to_numpy()
     nrn_pos = nrn_pos.to_numpy()
     nrn_lay = nodes.get(properties='layer')
-
+    
     # Load flatmap
-    assert len(flatmap_files) == 2, 'ERROR: Two flatmap files required: 1. xy mapping in a.u., 2. z (=depth) mapping in um'
     flatmap_atlas = Atlas.open(flatmap_path)
-    flatmap = flatmap_atlas.load_data(flatmap_files[0])
-    depths = flatmap_atlas.load_data(flatmap_files[1])
+    flatmap = flatmap_atlas.load_data(xy_file)
+    depths = flatmap_atlas.load_data(z_file)
     assert flatmap.raw.shape[:3] == depths.raw.shape, 'ERROR: Flatmap and depths map sizes inconsistent!'
     assert np.all(flatmap.voxel_dimensions == depths.voxel_dimensions), 'ERROR: Flatmap and depths map voxels inconsistent!'
     assert np.all(flatmap.bbox == depths.bbox), 'ERROR: Flatmap and depths map bounding boxes inconsistent!'
     
-    if xy_scale is None: # Scaling from a.u. to um
-        xy_scale = [flatmap.voxel_dimensions[0], flatmap.voxel_dimensions[1]] # Assume same pixel size in flat space as voxel size in atlas
+    if xy_scale is None: # x/y scaling from a.u. to um
+        xy_scale = [flatmap.voxel_dimensions[0], flatmap.voxel_dimensions[1]] # Default: Assume same pixel size in flat space as voxel size in atlas
     else:
-        assert np.array(xy_scale).size == 2 and np.all(np.array(xy_scale) > 0), 'ERROR: XY scale error!'
-
+        assert np.array(xy_scale).size == 2 and np.all(np.isfinite(xy_scale)) and np.all(np.array(xy_scale) != 0), 'ERROR: XY scale error!'
+    
+    if z_scale is None: # z scaling from a.u. to um
+        z_scale = 1.0 # Default: Assume depth values are already scaled to um
+    else:
+        assert np.array(z_scale).size == 1 and np.isfinite(z_scale) and z_scale != 0, 'ERROR: Z scale error!'
+    
+    print(f'INFO: Loaded x/y flatmap ("{xy_file}"; scale={np.round(xy_scale, decimals=2)}) and z (depth) map ("{z_file}"; scale={np.round(z_scale, decimals=2)}) from "{flatmap_path}"')
+    
     # Convert cell positions to flat space [Assume: missing values set to -1]
     flat_x = flatmap.lookup(nrn_pos)[:, 0].astype(float)
     flat_x[flat_x != -1] = flat_x[flat_x != -1] * xy_scale[0]
     flat_y = flatmap.lookup(nrn_pos)[:, 1].astype(float)
     flat_y[flat_y != -1] = flat_y[flat_y != -1] * xy_scale[1]
     flat_z = depths.lookup(nrn_pos).astype(float)
+    flat_z[flat_z != -1] = flat_z[flat_z != -1] * z_scale
     
     # Determine map indices/positions
     map_indices = flatmap.positions_to_indices(nrn_pos, keep_fraction=True) # Keep fractions within voxels => fraction x.5 corresponds to voxel center
@@ -99,9 +106,9 @@ def plot(out_dir, nrn_ids, nrn_lay, nrn_pos, model, model_inputs, model_params, 
                 pos_sel = pos[nrn_lay == lidx + 1, :]
                 plt.plot(pos_sel[:, 0], pos_sel[:, 1], pos_sel[:, 2], '.', color=lay_colors[lidx, :], markersize=1.0, alpha=0.5, label=f'L{lidx + 1}')
             ax.view_init(*v)
-            ax.set_xlabel('x [um]')
-            ax.set_ylabel('y [um]')
-            ax.set_zlabel('z [um]')
+            ax.set_xlabel('x [$\mu$m]')
+            ax.set_ylabel('y [$\mu$m]')
+            ax.set_zlabel('z [$\mu$m]')
             plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5), ncol=1)
             if vidx == 0:
                 plt.title(lbl + f'\n[N={len(nrn_ids)}cells]')
@@ -126,8 +133,8 @@ def plot(out_dir, nrn_ids, nrn_lay, nrn_pos, model, model_inputs, model_params, 
     plt.xlim((0, dist_max))
     plt.ylim((0, dist_max))
     plt.grid(True)
-    plt.xlabel('Distance in atlas space (data) [um]')
-    plt.ylabel('Distance in flat space (model) [um]')
+    plt.xlabel('Distance in atlas space (data) [$\mu$m]')
+    plt.ylabel('Distance in flat space (model) [$\mu$m]')
     plt.title(f'Cell distances in atlas vs. flat space [N={len(nrn_ids)}cells]')
     plt.tight_layout()
     
