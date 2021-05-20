f1b7dcc0847e9017eff0b4b6d73d48e2c08ba7ee
chr-pok <christoph.pokorny@epfl.ch>
2021-05-20 11:11:50 +0200

    Added arbitrary nodes population support to connection properties modeling

--- pipeline/model_building/conn_props.py
+++ pipeline/model_building/conn_props.py
@@ -11,38 +11,45 @@ import progressbar
 import numpy as np
 import matplotlib.pyplot as plt
 
-""" Extract statistics for synaptic properties from a sample of pairs of neurons """
+""" Extract statistics for synaptic properties between samples of neurons for each pair of m-types """
 def extract(circuit, min_sample_size_per_group=None, max_sample_size_per_group=None, **_):
     
-    nodes = circuit.nodes['All']
-    edges = circuit.edges['default']
-    m_types = sorted(nodes.property_values('mtype', is_present=True))
-    m_type_class = [nodes.get({'mtype': m}, properties='synapse_class').iloc[0] for m in m_types]
-    m_type_layer = [nodes.get({'mtype': m}, properties='layer').iloc[0] for m in m_types]
+    # Select edge population [assuming exactly one edge population in given edges file]
+    assert len(circuit.edges.population_names) == 1, 'ERROR: Only a single edge population per file supported for modelling!'
+    edges = circuit.edges[circuit.edges.population_names[0]]
+    
+    # Select corresponding source/target nodes populations
+    src_nodes = edges.source
+    tgt_nodes = edges.target
+    nodes = [src_nodes, tgt_nodes]
+    
+    m_types = [sorted(n.property_values('mtype', is_present=True)) for n in nodes]
+    m_type_class = [[nodes[i].get({'mtype': m}, properties='synapse_class').iloc[0] for m in m_types[i]] for i in range(len(nodes))]
+    m_type_layer = [[nodes[i].get({'mtype': m}, properties='layer').iloc[0] for m in m_types[i]] for i in range(len(nodes))]
     syn_props = list(filter(lambda x: not np.any([excl in x for excl in ['@', 'delay', 'afferent', 'efferent', 'spine_length']]), edges.property_names))
     
-    print(f'INFO: Estimating statistics for {len(syn_props)} properties between {len(m_types)}x{len(m_types)} m-types', flush=True)
+    print(f'INFO: Estimating statistics for {len(syn_props)} properties between {len(m_types[0])}x{len(m_types[1])} m-types', flush=True)
     
     # Statistics for #syn/conn
-    syns_per_conn_data = {'mean': np.full((len(m_types), len(m_types)), np.nan),
-                          'std': np.full((len(m_types), len(m_types)), np.nan),
-                          'min': np.full((len(m_types), len(m_types)), np.nan),
-                          'max': np.full((len(m_types), len(m_types)), np.nan)}
+    syns_per_conn_data = {'mean': np.full((len(m_types[0]), len(m_types[1])), np.nan),
+                          'std': np.full((len(m_types[0]), len(m_types[1])), np.nan),
+                          'min': np.full((len(m_types[0]), len(m_types[1])), np.nan),
+                          'max': np.full((len(m_types[0]), len(m_types[1])), np.nan)}
     
     # Statistics for synapse/connection properties
-    conn_prop_data = {'mean': np.full((len(m_types), len(m_types), len(syn_props)), np.nan), # Property value means across connections
-                      'std': np.full((len(m_types), len(m_types), len(syn_props)), np.nan), # Property value stds across connections
-                      'std-within': np.full((len(m_types), len(m_types), len(syn_props)), np.nan), # Property value stds across synapses within connections
-                      'min': np.full((len(m_types), len(m_types), len(syn_props)), np.nan), # Property value overall min
-                      'max': np.full((len(m_types), len(m_types), len(syn_props)), np.nan)} # Property value overall max
+    conn_prop_data = {'mean': np.full((len(m_types[0]), len(m_types[1]), len(syn_props)), np.nan), # Property value means across connections
+                      'std': np.full((len(m_types[0]), len(m_types[1]), len(syn_props)), np.nan), # Property value stds across connections
+                      'std-within': np.full((len(m_types[0]), len(m_types[1]), len(syn_props)), np.nan), # Property value stds across synapses within connections
+                      'min': np.full((len(m_types[0]), len(m_types[1]), len(syn_props)), np.nan), # Property value overall min
+                      'max': np.full((len(m_types[0]), len(m_types[1]), len(syn_props)), np.nan)} # Property value overall max
     
     # Extract statistics
     conn_counts = {'min': np.inf, 'max': -np.inf, 'sel': 0} # Count connections for reporting
     pbar = progressbar.ProgressBar()
-    for sidx in pbar(range(len(m_types))):
-        sids = nodes.ids({'mtype': m_types[sidx]})
-        for tidx in range(len(m_types)):
-            tids = nodes.ids({'mtype': m_types[tidx]})
+    for sidx in pbar(range(len(m_types[0]))):
+        sids = src_nodes.ids({'mtype': m_types[0][sidx]})
+        for tidx in range(len(m_types[1])):
+            tids = tgt_nodes.ids({'mtype': m_types[1][tidx]})
             edges_sel = edges.pathway_edges(sids, tids, ['@source_node', '@target_node'] + syn_props)
             if edges_sel.shape[0] == 0: # No synapses between pair of m-types
                 continue
@@ -78,7 +85,7 @@ def extract(circuit, min_sample_size_per_group=None, max_sample_size_per_group=N
             conn_prop_data['min'][sidx, tidx, :] = np.min(mins_within, 0)
             conn_prop_data['max'][sidx, tidx, :] = np.max(maxs_within, 0)
     
-    print(f'INFO: Between {conn_counts["min"]} and {conn_counts["max"]} connections per pathway found. {conn_counts["sel"]} of {len(m_types)**2} pathways selected.')
+    print(f'INFO: Between {conn_counts["min"]} and {conn_counts["max"]} connections per pathway found. {conn_counts["sel"]} of {len(m_types[0])}x{len(m_types[1])} pathways selected.')
     
     return {'syns_per_conn_data': syns_per_conn_data, 'conn_prop_data': conn_prop_data, 'm_types': m_types, 'm_type_class': m_type_class, 'm_type_layer': m_type_layer, 'syn_props': syn_props}
 
@@ -96,19 +103,19 @@ def build(syns_per_conn_data, conn_prop_data, m_types, m_type_class, m_type_laye
         for level in range(5):
             if level == 0: # Use source m-type/target layer/synapse class value, if existent
                 src_sel = [sidx]
-                tgt_sel = np.logical_and(np.array(m_type_layer) == m_type_layer[tidx], np.array(m_type_class) == m_type_class[tidx])
+                tgt_sel = np.logical_and(np.array(m_type_layer[1]) == m_type_layer[1][tidx], np.array(m_type_class[1]) == m_type_class[1][tidx])
             elif level == 1: # Use source m-type/target synapse class value, if existent
                 src_sel = [sidx]
-                tgt_sel = np.array(m_type_class) == m_type_class[tidx]
+                tgt_sel = np.array(m_type_class[1]) == m_type_class[1][tidx]
             elif level == 2: # Use per layer/synapse class value, if existent
-                src_sel = np.logical_and(np.array(m_type_layer) == m_type_layer[sidx], np.array(m_type_class) == m_type_class[sidx])
-                tgt_sel = np.logical_and(np.array(m_type_layer) == m_type_layer[tidx], np.array(m_type_class) == m_type_class[tidx])
+                src_sel = np.logical_and(np.array(m_type_layer[0]) == m_type_layer[0][sidx], np.array(m_type_class[0]) == m_type_class[0][sidx])
+                tgt_sel = np.logical_and(np.array(m_type_layer[1]) == m_type_layer[1][tidx], np.array(m_type_class[1]) == m_type_class[1][tidx])
             elif level == 3: # Use per synapse class value, if existent
-                src_sel = np.array(m_type_class) == m_type_class[sidx]
-                tgt_sel = np.array(m_type_class) == m_type_class[tidx]
+                src_sel = np.array(m_type_class[0]) == m_type_class[0][sidx]
+                tgt_sel = np.array(m_type_class[1]) == m_type_class[1][tidx]
             else: # Otherwise: Use overall value
-                src_sel = range(len(m_types))
-                tgt_sel = range(len(m_types))
+                src_sel = range(len(m_types[0]))
+                tgt_sel = range(len(m_types[1]))
             if np.any(np.isfinite(syns_per_conn_data['mean'][src_sel, :][:, tgt_sel])):
                 level_counts[f'Level{level}'] = level_counts.get(f'Level{level}', 0) + 1
                 break
@@ -127,10 +134,10 @@ def build(syns_per_conn_data, conn_prop_data, m_types, m_type_class, m_type_laye
     print(f'INFO: Interpolated {missing_list.shape[0]} missing values. Interpolation level counts: { {k: level_counts[k] for k in sorted(level_counts.keys())} }')
     
     # Create model dictionary (lookup-table)
-    prop_model_dict = {syn_props[p]: {m_types[s]: {m_types[t]: {k: conn_prop_model[k][s, t, p] for k in conn_prop_model.keys()} for t in range(len(m_types))} for s in range(len(m_types)) } for p in range(len(syn_props))}
-    prop_model_dict.update({'n_syn_per_conn': {m_types[s]: {m_types[t]: {k: syns_per_conn_model[k][s, t] for k in syns_per_conn_model.keys()} for t in range(len(m_types))} for s in range(len(m_types))}})
+    prop_model_dict = {syn_props[p]: {m_types[0][s]: {m_types[1][t]: {k: conn_prop_model[k][s, t, p] for k in conn_prop_model.keys()} for t in range(len(m_types[1]))} for s in range(len(m_types[0]))} for p in range(len(syn_props))}
+    prop_model_dict.update({'n_syn_per_conn': {m_types[0][s]: {m_types[1][t]: {k: syns_per_conn_model[k][s, t] for k in syns_per_conn_model.keys()} for t in range(len(m_types[1]))} for s in range(len(m_types[0]))}})
     
-    print(f'MODEL FIT for synapse/connection properties ({len(m_types)}x{len(m_types)} m-types):')
+    print(f'MODEL FIT for synapse/connection properties ({len(m_types[0])}x{len(m_types[1])} m-types):')
     print(list(prop_model_dict.keys()))
     
     return {'model': 'prop_model_dict[prop_name][src_type][tgt_type][stat_type]',
@@ -148,11 +155,11 @@ def plot(out_dir, syns_per_conn_data, conn_prop_data, m_types, syn_props, model,
     data_sel = 'mean' # Plot mean only
     for pidx, p in enumerate(prop_names):
         plt.figure(figsize=(8, 3), dpi=300)
-        for didx, data in enumerate([conn_prop_data[data_sel][:, :, pidx] if pidx < conn_prop_data[data_sel].shape[2] else syns_per_conn_data[data_sel], np.array([[model_fct(p, s, t, data_sel) for t in m_types] for s in m_types])]):
+        for didx, data in enumerate([conn_prop_data[data_sel][:, :, pidx] if pidx < conn_prop_data[data_sel].shape[2] else syns_per_conn_data[data_sel], np.array([[model_fct(p, s, t, data_sel) for t in m_types[1]] for s in m_types[0]])]):
             plt.subplot(1, 2, didx + 1)
             plt.imshow(data, interpolation='nearest', cmap='jet')
-            plt.xticks(range(len(m_types)), m_types, rotation=90, fontsize=3)
-            plt.yticks(range(len(m_types)), m_types, rotation=0, fontsize=3)
+            plt.xticks(range(len(m_types[1])), m_types[0], rotation=90, fontsize=3)
+            plt.yticks(range(len(m_types[1])), m_types[0], rotation=0, fontsize=3)
             plt.colorbar()
         plt.suptitle(p)
         plt.tight_layout()
