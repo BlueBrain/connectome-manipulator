7a5b8eb0a5c3920d718c5113b0047805587fd3fe
chr-pok <christoph.pokorny@epfl.ch>
2021-06-22 18:40:32 +0200

    Improved triad motif comparison

--- notebooks/helper_functions.py
+++ notebooks/helper_functions.py
@@ -60,7 +60,7 @@ def plot_topodb_comparison(db_dict, param_dict, groupby, out_dir, num_rows=1, sh
 
 
 """ Plot comparison of over-/underexpression of triad motifs from a single sample per circuit as specified (e.g., spec='Radius/All/0') """
-def plot_triads_comparison(topocomp_config, topocomp_config_files, spec, out_dir, show_fig=False):
+def plot_triads_comparison(topocomp_config, topocomp_config_files, spec, out_dir, plot_single_samples=True, show_fig=False, fig_format='png', fig_dpi=600):
     
     circuit_ids = sorted(topocomp_config['circuits'].keys())
     circuit_names = [topocomp_config['circuits'][cidx]['circuit_name'] for cidx in circuit_ids]
@@ -71,30 +71,57 @@ def plot_triads_comparison(topocomp_config, topocomp_config_files, spec, out_dir
     cfg_dict = dict([(k, Config(v)) for k, v in topocomp_config_files.items()])
     
     # Load triad overexpressions of selected sample
-    sample_type, sample_name, sample_nr = spec.split('/')
-    triads = []
+    spec_split = spec.split('/')
+    sample_type, sample_name = spec_split[:2]
+    if len(spec_split) == 3:
+        sample_nr = spec_split[2].split(',')
+    else:
+        sample_nr = []
+    triads = [] # Actual triad counts
+    triads_er = [] # ER control model counts
+    num_samples = []
     for cidx in circuit_ids:
         triad_fn = cfg_dict[cidx].stage('count_triads')['outputs']['triads']
         with open(triad_fn, 'r') as f:
-            triads.append(np.array(json.load(f)[sample_type][sample_name][sample_nr]['overexpression']))
+            triads_tmp = json.load(f)[sample_type][sample_name]
+        if len(sample_nr) == 0:
+            sample_nr = list(triads_tmp.keys())
+        else:
+            assert np.all(np.isin(sample_nr, list(triads_tmp.keys()))), 'ERROR: Sample number error!'
+        triads.append(np.array([triads_tmp[n]['overexpression'][0] for n in sample_nr]))
+        triads_er.append(np.array([triads_tmp[n]['overexpression'][1] for n in sample_nr]))
+        num_samples.append(len(sample_nr))
+    
+    if len(np.unique(num_samples)) == 1:
+        num_samples_str = str(num_samples[0])
+    else:
+        num_samples_str = '/'.join([str(n) for n in num_samples])
     
     # Rel. triad counts w.r.t. ER control model
-    triads_rel = [(triads[idx][0, :] - triads[idx][1, :]) / triads[idx][1, :] for idx in range(len(circuit_ids))]
+    triads_rel = [(triads[idx] - triads_er[idx]) / (triads[idx] + triads_er[idx]) for idx in range(len(circuit_ids))] # Normalize such that overexpression is constrained 0 < x <= 1 and underexpression to -1 <= x < 0
+    #triads_rel = [(triads[idx] - triads_er[idx]) / triads_er[idx] for idx in range(len(circuit_ids))]
     
     # Plot rel. triad overexpressions (normalized)
     markers = ['s', 'o']
+    colors = ['b', 'r']
+    minor_colors = ['lightsteelblue', 'lightcoral']
     plt.figure()
+    if plot_single_samples:
+        for idx in range(len(circuit_ids)):
+            plt.plot(range(1, triads_rel[idx].shape[1] + 1), triads_rel[idx].T, color=minor_colors[np.mod(idx, len(minor_colors))], linewidth=1, alpha=0.25, zorder=0)
     for idx in range(len(circuit_ids)):
-        plt.plot(range(1, len(triads_rel[0]) + 1), triads_rel[idx] / np.sum(triads_rel[idx]), marker=markers[np.mod(idx, len(markers))], label=circuit_names[idx])
-    plt.xticks(range(1, len(triads_rel[0]) + 1))
-    plt.title('Over-/underexpression of triad motifs (w.r.t. ER model)')
+        plt.errorbar(range(1, triads_rel[idx].shape[1] + 1), np.mean(triads_rel[idx], 0), yerr=np.std(triads_rel[idx], 0), color=colors[np.mod(idx, len(colors))], linewidth=3, elinewidth=2, marker=markers[np.mod(idx, len(markers))], label=circuit_names[idx])
+    plt.xlim(plt.xlim())
+    plt.plot(plt.xlim(), [0, 0], 'k', linewidth=0.5, zorder=0)
+    plt.xticks(range(1, triads_rel[0].shape[1] + 1))
+    plt.title(f'Triad motifs ({spec}; {num_samples_str} sample(s))')
     plt.xlabel('Triad motif')
-    plt.ylabel('Norm. rel. count')
-    plt.legend()
+    plt.ylabel('Norm. rel. count (w.r.t. ER model)')
+    plt.legend(loc='upper left', ncol=len(circuit_ids))
     
-    out_fn = os.path.abspath(os.path.join(out_dir, 'triad_comp-' + spec.replace('/', '_') + '.pdf'))
+    out_fn = os.path.abspath(os.path.join(out_dir, 'triad_comp-' + spec.replace('/', '_').replace(',', '_') + f'.{fig_format}'))
     print(f'Saving {out_fn}...')
-    plt.gcf().savefig(out_fn)
+    plt.gcf().savefig(out_fn, dpi=fig_dpi)
     if show_fig:
         plt.show()
     else:
