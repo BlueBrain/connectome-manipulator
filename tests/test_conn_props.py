6bc28cf83421cf2a8770ad858407dfde9957264b
Christoph Pokorny <christoph.pokorny@epfl.ch>
2022-11-11 17:48:17 +0100

    Added test cases for syn props model building

--- tests/test_conn_props.py
+++ tests/test_conn_props.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import pytest
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 from bluepysnap import Circuit
 from utils import TEST_DATA_DIR
@@ -68,11 +69,74 @@ def test_extract():
 
 
 def test_build():
-    
-#  'm_types': [['L4_MC', 'L4_PC', 'L5_PC'], ['L4_MC', 'L4_PC', 'L5_PC']],
-#  'm_type_class': [['INH', 'EXC', 'EXC'], ['INH', 'EXC', 'EXC']],
-#  'm_type_layer': [['LA', 'LB', 'LA'], ['LA', 'LB', 'LA']],
-#  'syn_props': ['conductance'],
-#     build(syns_per_conn_data, conn_prop_data, m_types, m_type_class, m_type_layer, syn_props, distr_types={}, data_types={}, data_bounds={}, **_):
+#     c = Circuit(os.path.join(TEST_DATA_DIR, 'circuit_sonata.json'))
+#     m_types = [list(c.nodes.property_values('mtype'))] * 2 # src/tgt mtypes
+#     m_type_class = [c.nodes.get({'mtype': m})['synapse_class'] for m in m_types[0]]
 
-    pass
+    m_types = [['L4_MC', 'L4_PC', 'L5_PC']] * 2
+    m_type_class = [['INH', 'EXC', 'EXC']] * 2
+    m_type_layer = [[4, 4, 5]] * 2
+
+    props = ['conductance', 'u_syn']
+    src_mtypes = m_types[0]
+    tgt_mtypes = m_types[1]
+    np.random.seed(0)
+    nsyn_data = {k: np.random.randint(low=1, high=10, size=(len(src_mtypes), len(tgt_mtypes))) for k in ['mean', 'std', 'min', 'max']}
+    prop_data = {k: np.random.rand(len(src_mtypes), len(tgt_mtypes), len(props)) for k in ['mean', 'std', 'std-within', 'min', 'max']}
+
+    # Check model building (default settings)
+    res = test_module.build(nsyn_data, prop_data, m_types, m_type_class, m_type_layer, props, distr_types={}, data_types={}, data_bounds={})
+    for sidx, s_mt in enumerate(src_mtypes):
+        for tidx, t_mt in enumerate(tgt_mtypes):
+            for pidx, pp in enumerate(props):
+                model_distr = res.get_distr_props(prop_name=pp, src_type=s_mt, tgt_type=t_mt)
+                assert model_distr['type'] == 'normal'
+                assert model_distr['mean'] == prop_data['mean'][sidx, tidx, pidx]
+                assert model_distr['std'] == prop_data['std'][sidx, tidx, pidx]
+                assert model_distr['std-within'] == prop_data['std-within'][sidx, tidx, pidx]
+            model_distr = res.get_distr_props(prop_name='n_syn_per_conn', src_type=s_mt, tgt_type=t_mt)
+            assert model_distr['mean'] == nsyn_data['mean'][sidx, tidx]
+            assert model_distr['std'] == nsyn_data['std'][sidx, tidx]
+
+    with pytest.raises(KeyError):
+        res.get_distr_props(prop_name='WRONG_NAME', src_type=s_mt, tgt_type=t_mt)
+    with pytest.raises(KeyError):
+        res.get_distr_props(prop_name='n_syn_per_conn', src_type='WRONG_TYPE', tgt_type=t_mt)
+    with pytest.raises(KeyError):
+        res.get_distr_props(prop_name='n_syn_per_conn', src_type=s_mt, tgt_type='WRONG_TYPE')
+
+    # Check distribution types
+    res = test_module.build(nsyn_data, prop_data, m_types, m_type_class, m_type_layer, props, distr_types={pp: 'truncnorm' for pp in props}, data_types={}, data_bounds={})
+    for sidx, s_mt in enumerate(src_mtypes):
+        for tidx, t_mt in enumerate(tgt_mtypes):
+            for pidx, pp in enumerate(props):
+                model_distr = res.get_distr_props(prop_name=pp, src_type=s_mt, tgt_type=t_mt)
+                assert model_distr['type'] == 'truncnorm'
+                assert model_distr['mean'] == prop_data['mean'][sidx, tidx, pidx]
+                assert model_distr['std'] == prop_data['std'][sidx, tidx, pidx]
+                assert model_distr['min'] == prop_data['min'][sidx, tidx, pidx]
+                assert model_distr['max'] == prop_data['max'][sidx, tidx, pidx]
+                assert model_distr['std-within'] == prop_data['std-within'][sidx, tidx, pidx]
+
+    with pytest.raises(AssertionError, match='ERROR: Distribution type "WRONG_TYPE" not supported!'):
+        test_module.build(nsyn_data, prop_data, m_types, m_type_class, m_type_layer, props, distr_types={pp: 'WRONG_TYPE' for pp in props}, data_types={}, data_bounds={})
+
+    # Check data types
+    res = test_module.build(nsyn_data, prop_data, m_types, m_type_class, m_type_layer, props, distr_types={}, data_types={pp: 'int' for pp in props[1:]}, data_bounds={})
+    for sidx, s_mt in enumerate(src_mtypes):
+        for tidx, t_mt in enumerate(tgt_mtypes):
+            for pidx, pp in enumerate(props):
+                model_distr = res.get_distr_props(prop_name=pp, src_type=s_mt, tgt_type=t_mt)
+                assert model_distr['dtype'] == 'int'
+                assert isinstance(res.draw(prop_name=pp, src_type=s_mt, tgt_type=t_mt)[0], np.int64)
+
+    # Check data bounds
+    # TODO
+
+    # Check synapse generation (w/o randomization)
+    # syn = res.apply()
+    # assert syn.shape[0] == 
+    # assert np.all([pp in syn.columns for pp in props])
+
+    # Check interpolation
+    # TODO
