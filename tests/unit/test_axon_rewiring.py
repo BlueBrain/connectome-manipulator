a578a57a347bfa12de6eb3410f0dfd37b0845b91
Luc Dominic Grosheintz-Laval <luc.grosheintz-laval@epfl.ch>
2023-06-29 08:23:08 +0000

    Remove `connectome_manipulation/axon_rewiring.py`.
    This functionality is currently not used and there are no plans to use it in the future.

--- tests/unit/test_axon_rewiring.py
+++
@@ -1,5 +0,0 @@
-import connectome_manipulator.connectome_manipulation.axon_rewiring as test_module
-
-
-def test_null():
-    pass
--- tests/unit/test_axon_shuffling.py
+++
@@ -1,5 +0,0 @@
-import connectome_manipulator.connectome_manipulation.axon_shuffling as test_module
-
-
-def test_null():
-    pass
