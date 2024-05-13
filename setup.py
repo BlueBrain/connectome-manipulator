fa56fec987dd098f261ff3d0a61fc1f59e0597ba
Christoph Pokorny <christoph.pokorny@epfl.ch>
2024-05-13 13:47:02 +0200

    Pinned dependencies

--- setup.py
+++ setup.py
@@ -28,29 +28,23 @@ setup(
     description="A connectome manipulation framework for SONATA circuits",
     long_description=README,
     long_description_content_type="text/x-rst",
-    url="https://bbpgitlab.epfl.ch/conn/structural/connectome_manipulator.git",
-    project_urls={
-        "Tracker": "https://bbpteam.epfl.ch/project/issues/projects/SSCXDIS/issues",
-        "Source": "https://bbpgitlab.epfl.ch/conn/structural/connectome_manipulator.git",
-    },
+    url="https://github.com/BlueBrain/connectome-manipulator.git",
     license="Apache-2",
     install_requires=[
-        "bluepysnap>=3.0.1",
-        "numpy",
-        "pandas",
-        "progressbar",
-        "pyarrow",
-        "scipy",
-        "scikit-learn",
-        "voxcell",
-        "pyarrow",
-        "scikit-learn",
-        "tables",  # Optional dependency of pandas.DataFrame.to_hdf()
-        "distributed",  # Dask
-        "dask-mpi",
+        "bluepysnap==3.0.1",
+        "numpy==1.24.3",
+        "pandas==2.0.2",
+        "progressbar==2.5",
+        "pyarrow==12.0.1",
+        "scipy==1.10.1",
+        "scikit-learn==1.2.2",
+        "voxcell==3.1.5",
+        "tables==3.8.0",  # Optional dependency of pandas.DataFrame.to_hdf()
+        "distributed==2023.6.0",  # Dask
+        "dask-mpi==2022.4.0",
     ],
     packages=find_packages(),
-    python_requires=">=3.7",
+    python_requires="==3.10.8",
     extras_require={"docs": ["sphinx", "sphinx-bluebrain-theme"]},
     entry_points={
         "console_scripts": [
@@ -64,9 +58,6 @@ setup(
         "Intended Audience :: Science/Research",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
