c9ceb07f2bf191de06299fbf2d6a50da35bea8f5
Mustafa Kerem Kurban <kerem.kurban@epfl.ch>
2024-05-29 20:27:22 +0200

    update str comp for multipop

---
+++ examples/remove_connections/sscx/run_struct_comparison.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --job-name=struct_comp
+#SBATCH --time=1:00:00
+#SBATCH --account=proj142
+#SBATCH --partition=prod
+#SBATCH --mem=0
+#SBATCH --exclusive
+#SBATCH --constraint=cpu
+#SBATCH --out=logs/%j.txt
+#SBATCH --err=logs/%j.txt
+
+module purge
+module load unstable
+#source /gpfs/bbp.cscs.ch/home/pokorny/ReWiringKernel/bin/activate
+source /gpfs/bbp.cscs.ch/project/proj112/home/kurban/christoph_paper/github/venv_3_10_8/bin/activate
+connectome-manipulator compare-connectomes $1 $2 $3
+
+# EXAMPLE HOW TO RUN: sbatch run_struct_comparison.sh <model_config.json> [--force-recomp-circ1] [--force-recomp-circ2]
+# e.g. sbatch run_struct_comparison.sh structcomp_config.json --force-recomp-circ1 --force-recomp-circ2
