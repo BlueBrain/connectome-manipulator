053b800acba2accc2eebefc1c89fc8366c93fd71
Christoph Pokorny <christoph.pokorny@epfl.ch>
2024-07-10 13:14:04 +0200

    Added functional test for parquet2hdf5

---
+++ tests/functional/test_parquet2hdf5.py
@@ -0,0 +1,18 @@
+# This file is part of connectome-manipulator.
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2024 Blue Brain Project/EPFL
+
+import os
+import subprocess
+
+
+def test_parquet2hdf5():
+    """Test if parquet2hdf5 function is available. Will raise an FileNotFoundError if not."""
+    with subprocess.Popen(
+        ["parquet2hdf5", "--version"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        env={"PATH": os.getenv("PATH", "")},
+    ) as proc:
+        print("parquet2hdf5 v" + proc.communicate()[0].decode())
